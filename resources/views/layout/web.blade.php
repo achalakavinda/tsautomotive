85a85c9a18b74723c68b7ece0ae6076423de4e00
Achala Kavinda <achalakavinda95@gmail.com>
2024-11-04 17:59:42 +0800

    add changes

---
+++ resources/views/layout/web.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+  <head>
+    <!-- Meta Tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="ThemeServices">
+    <!-- Favicon Icon -->
+    <link rel="icon" href="assets/img/favicon.png">
+    <!-- Site Title -->
+    <title>Welcome | TS Automotive - Malaga</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/fontawesome.min.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" href="assets/css/slick.min.css">
+    <link rel="stylesheet" href="assets/css/odometer.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    @yield('css')
+  </head>
+  <body>
+    <!-- Start Preloader -->
+    <div class="cs_center cs_heading_bg" id="preloader">
+      <div class="preloader-inner">
+        <div class="spinner">
+          <img src="assets/img/preloader_logo.svg" alt="img">
+          <img src="assets/img/icons/wheel.svg" alt="img" class="wheel">
+        </div>
+        <div class="loading-text">
+          <img src="assets/img/logo.svg" alt="img">
+        </div>
+      </div>
+    </div>
+    <!-- End Preloader -->
+    
+    <!-- Start Header Section -->
+        @include('components.header')
+    <!-- End Header Section -->
+
+
+        @yield('content')
+
+
+    <!-- Start Footer -->
+        @include('components.footer')
+    <!-- End Footer -->
+    
+
+    <!-- Start Scroll Top Button -->
+    <div class="cs_scroll_top_btn cs_accent_bg cs_white_color cs_center cs_radius_50">
+      <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 32 32"><path fill="currentColor" d="M16 4L6 14l1.41 1.41L15 7.83V28h2V7.83l7.59 7.58L26 14z"/></svg>
+    </div>
+    <!-- End Scroll Top Button -->
+
+
+    <!-- Script -->
+    <script src="assets/js/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/jquery.slick.min.js"></script>
+    <script src="assets/js/ripples.min.js"></script>
+    <script src="assets/js/odometer.js"></script>
+    <script src="assets/js/main.js"></script>
+    @yield('js')
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/services', function () {
+    return view('service');
+});
+
+Route::get('/about', function () {
+    return view('aboutUs');
+});
+
+Route::get('/contact-us', function () {
+    return view('contactUs');
 });
